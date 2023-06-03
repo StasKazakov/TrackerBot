1a128119468c888d9d1cb8b01966982f1d66fab7
CBoYXD <135316445+CBoYXD@users.noreply.github.com>
2023-06-03 15:34:45 +0300

    Add files via upload
    Add Database

---
+++ db.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+class Database:
+    
+    
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+         
+    
+    def add_user(self, user_id, name_tg):
+        with self.connection:
+            result = self.cursor.execute("SELECT user_id FROM user_lang WHERE user_id = ?", (user_id,)).fetchall()
+            if bool(len(result)) == False:
+                return self.connection.execute("INSERT INTO user_lang (user_id, name_tg) VALUES (?,?)", (user_id, name_tg ))
+            else:
+                pass   
+    
+    
+    def save_language(self, language, user_id):
+        with self.connection:
+            return self.connection.execute("UPDATE user_lang  SET user_language = ? WHERE user_id = ?", (language, user_id ))
+    
+    
+    def check_language(self, user_id):
+        with self.connection:
+            res = self.cursor.execute("SELECT user_language FROM user_lang WHERE user_id = ?", (user_id,)).fetchone()
+            return ''.join(res)
+        
+    
+    
+    def save_link(self, user_id, root_link, track_link):
+        # This function is not complete, will change in future commits
+        with self.connection:
+            return self.cursor.execute("INSERT INTO keys (user_id, root_link, track_link) VALUES (?,?,?)", (user_id, root_link, track_link))
+        
