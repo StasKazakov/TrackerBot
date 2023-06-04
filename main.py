5ff512627a9a9bf22f47f55c9e736ed2476bc8b4
hush-tukul <hush-tukul@protonmail.com>
2023-06-04 12:29:15 +0200

    Пододава різні приколи....зокрема додав: - система логгінгу кольорова й з нормальним відораженням статусу боту - файл конфігу який виконує функцію будування конфігу з енв даних - ну й перекинув наш редірект не на фласку а на його асинх версії до мейну

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import logging
 import asyncio
 import os
+from urllib.parse import urlparse, parse_qs
+
+import betterlogging as bl
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.filters import Text
+from quart import Quart, redirect
+
+from config import load_config
+from redirection.url_flask import UTMTracker
+from services import broadcaster
 from tools.keyboard import start_menu, main_menu
 from tools.db import Database
 
-
+logger = logging.getLogger(__name__)
+log_level = logging.INFO
+bl.basic_colorized_config(level=log_level)
+app = Quart(__name__)
 load_dotenv()
-logging.basicConfig(filename='all_log.log', level=logging.INFO, format='%(asctime)s - %(levelname)s -%(message)s')
-bot = Bot(token=os.getenv('TOKEN_past'))
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
 dp = Dispatcher()
 db = Database("TrackerBot.db")
+
+
+async def on_startup(bot: Bot, admin_ids: list[int]):
+    await broadcaster.broadcast(bot, admin_ids, "Бот був запущений")
+
 
 
 @dp.message(Command('start'))
@@ -57,8 +73,28 @@ async def answer_menu(callback:types.CallbackQuery):
 
 
 async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+    config = load_config(".env")
+    await on_startup(bot, config.tg_bot.admin_ids)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
     asyncio.run(main())
+    @app.route('/', methods=['GET'])
+    async def handle_request():
+        g = ['utm_source', 'utm_medium', 'utm_campaign', 'datetime']
+        redirect_url = UTMTracker('https://t.me/botfatherdev', 'Telegram', 'adv', 'new service').add_utm_params()
+        parsed_url = urlparse(redirect_url)
+        query_params = parse_qs(parsed_url.query)
+        params_list = [f"{i}: {query_params.get(i, [''])[0]}" for i in g]
+        print(*params_list, sep='\n')
+        return redirect(parsed_url.geturl())
+
+
+    if __name__ == '__main__':
+        asyncio.run(main())
+        app.run(host='0.0.0.0', port=8000)
