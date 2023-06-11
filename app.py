241ab2a0d21f458fec71bcc11217676776855ac8
hush-tukul <hush-tukul@protonmail.com>
2023-06-11 16:53:57 +0200

    READY 2.1

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from urllib.parse import urlparse, parse_qs
 
 from quart import request, redirect, Quart
 
-from db import get_link
-from tools.db import Database
+from run import db
 
 app = Quart(__name__)
 
@@ -16,7 +15,7 @@ async def handle_request():
     link_id = query_params.get('link_id', 0)
     '''ТУТ ПОДКЛЮЧЕНИЕ К БД ЧТОБЫ ВЫТАЩИТЬ МЕТОДОМ КЛАССА 
     БД ССЫЛКУ НА КОТОРУЮ НУЖНО ПЕРЕНАПРАВИТЬ ФЛАСК'''
-    orig_link = await Database.get_user_link(link_id)
+    orig_link = await db.get_user_link(link_id)
     logging.info(orig_link)
     '''здесь планируется передавать в БД линк айди и получать оригинальную ссылку чтобы на нее перейти'''
     return redirect(orig_link)
